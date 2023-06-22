60bae33f023a74b71706ae84b21fb6e1d4e6020d
Nathan Walker <nswalker@gmail.com>
2023-06-21 18:19:37 -0700

    password reset update

--- app/routes/password-reset/index.tsx
+++ app/routes/password-reset/index.tsx
@@ -1,6 +1,11 @@
 import { json, redirect } from "@remix-run/node";
 import { Form, useActionData } from "@remix-run/react";
-import { createPasswordResetToken, createUser,getUserByEmail, getUserById,   } from "~/models/user.server";
+import {
+  createPasswordResetToken,
+  createUser,
+  getUserByEmail,
+  getUserById,
+} from "~/models/user.server";
 import { getUserId, createUserSession } from "~/session.server";
 import type {
   ActionFunction,
@@ -16,25 +21,22 @@ export const loader: LoaderFunction = async ({ request }) => {
   return json({});
 };
 
-
 interface ActionData {
   errors: {
     email?: string;
     password?: string;
-    id?:string;
- 
+    id?: string;
   };
 }
 
-
 export const action: ActionFunction = async ({ request }) => {
-    const queryParams = new URLSearchParams(request.url.split('?')[1]);
-  const id = parseInt(queryParams.get('id') || '', 10);
-    const formData = await request.formData();
-    const email = formData.get("email"); 
-    const password = formData.get("password");
-    const token = queryParams.get('token');
-  
+  const queryParams = new URLSearchParams(request.url.split("?")[1]);
+  const id = parseInt(queryParams.get("id") || "", 10);
+  const formData = await request.formData();
+  const email = formData.get("email");
+  const password = formData.get("password");
+  const token = queryParams.get("token");
+
   if (!token) {
     // Token is missing, handle the error
     return redirect("/error");
@@ -42,7 +44,7 @@ export const action: ActionFunction = async ({ request }) => {
 
   const passwordReset = await createPasswordResetToken(token);
 
-  if (!passwordReset || (passwordReset.expiresAt)) {
+  if (!passwordReset || passwordReset.expiresAt) {
     // Invalid token or expired token, handle the error
     return redirect("/error");
   }
@@ -59,17 +61,12 @@ export const action: ActionFunction = async ({ request }) => {
     return redirect("/error");
   }
 
-  
-
-
- 
-
-    if (!validateEmail(email)) {
-      return json<ActionData>(
-        { errors: { email: "Email is invalid" } },
-        { status: 400 }
-      );
-    }
+  if (!validateEmail(email)) {
+    return json<ActionData>(
+      { errors: { email: "Email is invalid" } },
+      { status: 400 }
+    );
+  }
 
   if (typeof password !== "string" || password.length === 0) {
     return json<ActionData>(
@@ -94,20 +91,17 @@ export const action: ActionFunction = async ({ request }) => {
   }
 
   return json({ user });
-
-}
-
-
-
-
-
-
+};
 
 export default function ResetPasswordForm() {
   const actionData = useActionData();
 
   if (actionData?.success) {
-    return <p>Password reset successful. You can now log in with your new password.</p>;
+    return (
+      <p>
+        Password reset successful. You can now log in with your new password.
+      </p>
+    );
   }
 
   return (
