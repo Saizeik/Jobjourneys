b78d531b15e259f99bd24fcb724f0b9fc5b756dc
Nathan Walker <nswalker@gmail.com>
2023-07-16 14:58:35 -0700

    password reset update

--- app/routes/password-reset/index.tsx
+++ app/routes/password-reset/index.tsx
@@ -3,17 +3,11 @@ import { Form, useActionData, useSearchParams, Link } from "@remix-run/react";
 import * as React from "react";
 import { useEffect, useState } from "react";
 import { motion } from "framer-motion";
-import { safeRedirect, } from "~/utils";
+import { safeRedirect } from "~/utils";
 import { loginImages } from "../../loginImages";
-import {
-  getUserByEmail,
-  updatePassword,
-} from "~/models/user.server";
+import { getUserByEmail, updatePassword } from "~/models/user.server";
 import { getUserId } from "~/session.server";
-import type {
-  ActionFunction,
-  LoaderFunction,
-} from "@remix-run/node";
+import type { ActionFunction, LoaderFunction } from "@remix-run/node";
 import { prisma } from "~/db.server";
 import { toast, Toaster } from "react-hot-toast";
 
@@ -115,19 +109,18 @@ export const action: ActionFunction = async ({ request }) => {
     where: { id: passwordReset.id },
   });
 
-   // Append the success flag to the redirectTo URL
-   const redirectTo = `${safeRedirect(formData.get("redirectTo"), "/login")}?success=true`;
+  // Append the success flag to the redirectTo URL
+  const redirectTo = `${safeRedirect(
+    formData.get("redirectTo"),
+    "/login"
+  )}?success=true`;
 
-   // Show the toast message on successful password reset
-   toast.success("Password reset successful!");
+  // Show the toast message on successful password reset
+  toast.success("Password reset successful!");
 
-
-   setTimeout(() => {
-    redirect(redirectTo);
+  setTimeout(() => {
+    return redirect(redirectTo);
   }, 3000);
-
-  // Return an empty response, as the redirect will happen after the timeout
-  return {};
 };
 export default function ResetPasswordForm() {
   const emailRef = React.useRef<HTMLInputElement>(null);
@@ -162,163 +155,172 @@ export default function ResetPasswordForm() {
 
   return (
     <>
-    <main className="relative flex min-h-screen items-center justify-center overflow-y-auto bg-white">
-      <div className="relative sm:pb-16 sm:pt-8">
-        <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
-          <div className="relative shadow-xl sm:overflow-hidden sm:rounded-2xl">
-            <div className=" absolute inset-0">
-              {randomImage && (
-                <motion.img
-                  initial={{ opacity: 0 }}
-                  animate={{
-                    opacity: imageLoading ? 0 : 1,
-                  }}
-                  transition={{ delay: 0, duration: 2 }}
-                  onLoad={imageLoaded}
-                  src={randomImage.src}
-                  alt={randomImage.alt}
-                  className="h-full w-full object-cover"
-                />
-              )}
-              <div className="absolute inset-0 bg-teal-400 mix-blend-multiply" />
-            </div>
-            <div className=" lg:pb-18 relative px-4 pb-8 pt-16 sm:px-6 sm:pb-14 sm:pt-24 lg:px-8 lg:pt-32">
-              <h1 className="text-center text-6xl font-extrabold tracking-tight sm:text-8xl lg:text-9xl">
-                <span className="mb-4 block uppercase text-white drop-shadow-md">
-                  Job Journey
-                </span>
-              </h1>
-              <div className="flex min-h-full flex-col justify-center">
-                <div className="mx-auto w-full max-w-md px-8">
-                  <Form method="post" className="space-y-6">
-                    <div>
+      <Toaster position="top-center" />
+      <main className="relative flex min-h-screen items-center justify-center overflow-y-auto bg-white">
+        <div className="relative sm:pb-16 sm:pt-8">
+          <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
+            <div className="relative shadow-xl sm:overflow-hidden sm:rounded-2xl">
+              <div className=" absolute inset-0">
+                {randomImage && (
+                  <motion.img
+                    initial={{ opacity: 0 }}
+                    animate={{
+                      opacity: imageLoading ? 0 : 1,
+                    }}
+                    transition={{ delay: 0, duration: 2 }}
+                    onLoad={imageLoaded}
+                    src={randomImage.src}
+                    alt={randomImage.alt}
+                    className="h-full w-full object-cover"
+                  />
+                )}
+                <div className="absolute inset-0 bg-teal-400 mix-blend-multiply" />
+              </div>
+              <div className=" lg:pb-18 relative px-4 pb-8 pt-16 sm:px-6 sm:pb-14 sm:pt-24 lg:px-8 lg:pt-32">
+                <h1 className="text-center text-6xl font-extrabold tracking-tight sm:text-8xl lg:text-9xl">
+                  <span className="mb-4 block uppercase text-white drop-shadow-md">
+                    Job Journey
+                  </span>
+                </h1>
+                <div className="flex min-h-full flex-col justify-center">
+                  <div className="mx-auto w-full max-w-md px-8">
+                    <Form method="post" className="space-y-6">
                       <div>
+                        <div>
+                          <label
+                            htmlFor="email"
+                            className="block text-sm font-bold text-white"
+                          >
+                            Email Address
+                          </label>
+                          <div className="mt-2">
+                            <input
+                              id="email"
+                              name="email"
+                              type="email"
+                              ref={emailRef}
+                              autoComplete="email"
+                              aria-invalid={
+                                actionData?.errors?.email ? true : undefined
+                              }
+                              aria-describedby="email-error"
+                              className="w-full rounded border border-gray-500 px-2 py-1 my-2 text-lg"
+                            />
+                            {actionData?.errors?.email && (
+                              <div
+                                className="pt-1 text-red-700"
+                                id="email-error"
+                              >
+                                {actionData.errors.email}
+                              </div>
+                            )}
+                          </div>
+                        </div>
+
                         <label
-                          htmlFor="email"
+                          htmlFor="password"
                           className="block text-sm font-bold text-white"
                         >
-                          Email Address
+                          Password
                         </label>
                         <div className="mt-1">
                           <input
-                            id="email"
-                            name="email"
-                            type="email"
-                            ref={emailRef}
-                            autoComplete="email"
+                            type="hidden"
+                            name="token"
+                            value={actionData?.token}
+                          />
+                          <input
+                            id="password"
+                            ref={passwordRef}
+                            name="password"
+                            type="password"
+                            autoComplete="current-password"
                             aria-invalid={
-                              actionData?.errors?.email ? true : undefined
+                              actionData?.errors?.password ? true : undefined
                             }
-                            aria-describedby="email-error"
+                            aria-describedby="password-error"
                             className="w-full rounded border border-gray-500 px-2 py-1 text-lg"
                           />
-                          {actionData?.errors?.email && (
-                            <div className="pt-1 text-red-700" id="email-error">
-                              {actionData.errors.email}
+                          {actionData?.errors?.password && (
+                            <div
+                              className="pt-1 text-red-700"
+                              id="password-error"
+                            >
+                              {actionData.errors.password}
                             </div>
                           )}
                         </div>
                       </div>
 
-                      <label
-                        htmlFor="password"
-                        className="block text-sm font-bold text-white"
-                      >
-                        Password
-                      </label>
-                      <div className="mt-1">
-                        <input
-                          type="hidden"
-                          name="token"
-                          value={actionData?.token}
-                        />
-                        <input
-                          id="password"
-                          ref={passwordRef}
-                          name="password"
-                          type="password"
-                          autoComplete="current-password"
-                          aria-invalid={
-                            actionData?.errors?.password ? true : undefined
-                          }
-                          aria-describedby="password-error"
-                          className="w-full rounded border border-gray-500 px-2 py-1 text-lg"
-                        />
-                        {actionData?.errors?.password && (
-                          <div
-                            className="pt-1 text-red-700"
-                            id="password-error"
-                          >
-                            {actionData.errors.password}
-                          </div>
-                        )}
+                      <div>
+                        <label
+                          htmlFor="confirmPassword"
+                          className="block text-sm font-bold text-white"
+                        >
+                          Confirm Password
+                        </label>
+                        <div className="mt-1">
+                          <input
+                            id="confirmPassword"
+                            ref={confirmPasswordRef}
+                            name="confirmPassword"
+                            type="password"
+                            autoComplete="current-password"
+                            aria-invalid={
+                              actionData?.errors?.confirmPassword
+                                ? true
+                                : undefined
+                            }
+                            aria-describedby="confirmPassword-error"
+                            className="w-full rounded border border-gray-500 px-2 py-1 text-lg"
+                          />
+                          {actionData?.errors?.confirmPassword && (
+                            <div
+                              className="pt-1 text-red-700"
+                              id="confirmPassword-error"
+                            >
+                              {actionData.errors.confirmPassword}
+                            </div>
+                          )}
+                        </div>
                       </div>
-                    </div>
 
-                    <div>
-                      <label
-                        htmlFor="confirmPassword"
-                        className="block text-sm font-bold text-white"
+                      <input
+                        type="hidden"
+                        name="redirectTo"
+                        value={redirectTo}
+                      />
+                      <button
+                        type="submit"
+                        className="bg-custom-newColor hover:bg-custom-spaceBlack focus:bg-custom-spaceBlack hover: w-full rounded px-4 py-2 font-medium text-white text-white"
                       >
-                        Confirm Password
-                      </label>
-                      <div className="mt-1">
-                        <input
-                          id="confirmPassword"
-                          ref={confirmPasswordRef}
-                          name="confirmPassword"
-                          type="password"
-                          autoComplete="current-password"
-                          aria-invalid={
-                            actionData?.errors?.confirmPassword
-                              ? true
-                              : undefined
-                          }
-                          aria-describedby="confirmPassword-error"
-                          className="w-full rounded border border-gray-500 px-2 py-1 text-lg"
-                        />
-                        {actionData?.errors?.confirmPassword && (
-                          <div
-                            className="pt-1 text-red-700"
-                            id="confirmPassword-error"
-                          >
-                            {actionData.errors.confirmPassword}
-                          </div>
-                        )}
-                      </div>
-                    </div>
+                        Reset Password
+                      </button>
+                      {showSuccessMessage && (
+                        <div className="bg-white p-2 text-center text-green-800">
+                          Password reset successful!
+                        </div>
+                      )}
 
-                    <input type="hidden" name="redirectTo" value={redirectTo} />
-                    <button
-                      type="submit"
-                      className="bg-custom-newColor hover:bg-custom-spaceBlack focus:bg-custom-spaceBlack   hover: w-full rounded px-4 py-2 font-medium text-white text-white"
-                    >
-                      Reset Password
-                    </button>
-                    {showSuccessMessage && (
-        <div className="bg-green-200 p-2 text-center text-green-800">
-          Password reset successful!
-        </div>
-      )}
-                
-                     <div className="flex items-center justify-center">
-              <div className="text-center text-sm font-bold text-white">
-                Remember your password?{" "}
-                <Link className="text-lg font-bold text-white underline" to="/login">
-                  Log in
-                </Link>
-              </div>
-            </div>
-                  </Form>
+                      <div className="flex items-center justify-center">
+                        <div className="text-center text-sm font-bold text-white">
+                          Remember your password?{" "}
+                          <Link
+                            className="text-lg font-bold text-white underline"
+                            to="/login"
+                          >
+                            Log in
+                          </Link>
+                        </div>
+                      </div>
+                    </Form>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-      
-    </main>
-    <Toaster position="top-center" />
+      </main>
     </>
   );
 }
